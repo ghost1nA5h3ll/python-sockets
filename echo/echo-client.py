89b907bc87646fb33d37ce243f355db00f36e3c7
Jonas Russer <xyz@xyz.de>
2021-04-28 21:49:54 +0200

    add comments

--- echo/echo-client.py
+++ echo/echo-client.py
@@ -2,13 +2,15 @@
 
 import socket
 
-HOST = '127.0.0.1'
+#configure target IP and port
+HOST = '127.0.0.1' 
 PORT = 17345
 
+#create a socket object (IPv4 with TCP)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'hello my server')
-    data = s.recv(1024)
+    s.connect((HOST, PORT)) #connect to echo-server
+    s.sendall(b'hello my server') #send data to server
+    data = s.recv(1024) #get answer from server
 
-print('Received from server: {}'.format(repr(data)))
+print('Received from server: {}'.format(repr(data))) #print the data to the commandline
 
